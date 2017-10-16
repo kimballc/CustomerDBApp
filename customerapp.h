7a446585f6868b035cf1f057c46fbf8589e059c0
Christian Kimball <christian.kimball@cgi.com>
2017-10-16 11:23:05 -0400

    Initial Commit lab 4

--- customerapp.h
+++ customerapp.h
@@ -2,6 +2,7 @@
 #define CUSTOMERAPP_H
 
 #include <QMainWindow>
+#include <string>
 
 namespace Ui {
 class CustomerApp;
@@ -15,8 +16,19 @@ public:
     explicit CustomerApp(QWidget *parent = 0);
     ~CustomerApp();
 
+private slots:
+    void on_updateButton_clicked();
+
+    void on_actionExit_triggered();
+
 private:
     Ui::CustomerApp *ui;
+    // opens a connection to the database & retrieves your customer info
+    void connectToDb();
+    // updates your customer record in the database
+    void updateDb(QString &lastname, QString &email,
+                      QString &city, QString &state);
+
 };
 
 #endif // CUSTOMERAPP_H
